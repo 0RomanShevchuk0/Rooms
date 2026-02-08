4cff587f14adafa6e284683afa4519eb986c8ee2
0RomanShevchuk0 <0romanshevchuk0@gmail.com>
2026-02-08 15:46:14 +0100

    Set global API prefix for the application

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.setGlobalPrefix('api');
+
 	const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') ?? [];
 	app.enableCors({
 		origin: [...allowedOrigins],
